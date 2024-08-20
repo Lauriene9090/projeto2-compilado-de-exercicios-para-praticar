47c9f5853799b57148fa38dabd5443d1a1d4fc68
Lauriene9090 <laurienea840@gmail.com>
2024-08-20 15:11:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1 Faça um Programa que mostre a mensagem "Olá mundo" na tela//
+window.alert = prompt('ola mundo')
+
+//Faça um Programa que peça um número e então mostre a mensagem "O número informado foi [número]".
+
